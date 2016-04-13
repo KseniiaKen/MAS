0bd7345a7e16bf73778b1f18a02b803ce16011bc
U-My_computer\\user <kseniia.vlasova.910@gmail.com>
2016-04-13 20:01:10 +0300

    Forgotten file
    Signed-off-by: KseniiaKen <ksand910@gmail.com>

---
+++ CoreAMS/Messages/ResultsMessage.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreAMS.Messages
+{
+    [Serializable]
+    public class ResultsMessage: Message
+    {
+        public int suspectableCount;
+        public int recoveredCount;
+        public int infectiousCount;
+        public int funeralCount;
+        public int deadCount;
+        public int time;
+
+        public ResultsMessage(Guid senderId, int suspectableCount, int recoveredCount, int infectiousCount, int funeralCount, int deadCount, int time) : base(senderId, MessageType.Results)
+        {
+            this.suspectableCount = suspectableCount;
+            this.recoveredCount = recoveredCount;
+            this.infectiousCount = infectiousCount;
+            this.funeralCount = funeralCount;
+            this.deadCount = deadCount;
+            this.time = time;
+        }
+    }
+}
