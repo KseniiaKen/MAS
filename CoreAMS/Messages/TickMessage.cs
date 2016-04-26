f85a5d90aaf9bd3480d132cfd0a53a8afe805589
U-My_computer\\user <kseniia.vlasova.910@gmail.com>
2016-04-26 18:58:36 +0300

    Forgotten file
    Signed-off-by: KseniiaKen <ksand910@gmail.com>

---
+++ CoreAMS/Messages/TickMessage.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoreAMS.Messages
+{
+    [Serializable]
+    public class TickMessage: Message
+    {
+        public AddAgentMessage[] agents;
+
+        public TickMessage(Guid senderId, AddAgentMessage[] agents) : base(senderId, MessageType.Tick)
+        {
+            this.agents = agents;
+        }
+    }
+}
